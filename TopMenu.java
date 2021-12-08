3ddbaede4903d7ca074d39dc9c1937ff2cf86843
Влад <eremeev.vlad.v@yandex.ru>
2021-12-09 00:25:27 +0300

    0.2"Полностью готовая 1 лаба"

--- TopMenu.java
+++ TopMenu.java
@@ -1,6 +1,9 @@
+import java.io.Console;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.LogManager;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JMenuBar;
 
@@ -8,30 +11,41 @@ public class TopMenu {
     List<TopMenuElement> elements = new ArrayList<TopMenuElement>();
 
     JMenuBar menuBar = new JMenuBar();
+
     public JMenuBar getMenuBar() {
         return menuBar;
     }
 
-    public TopMenu(){
-        //Создаем вкладку создания файлов
-         TopMenuElement createNewFile = new TopMenuElement(0,4);
-         createNewFile.CreateMenuElement(new String("Создание файлов"),
-            new String[]{}, new String[]{"Формат TXT", "Формат World", "Формат Excel","Формат CSV"} 
-         );
-         elements.add(createNewFile);
+    public TopMenu() {
+        // Создаем вкладку создания файлов
+        TopMenuElement createNewFile = new TopMenuElement(0, 4);
+        createNewFile.CreateMenuElement(new String("Создание файлов"),
+                new String[] {},
+                new String[] { "Формат TXT", "Формат World", "Формат Excel", "Формат CSV" });
+        elements.add(createNewFile);
 
         TopMenuElement openFile = new TopMenuElement(4, 3);
         openFile.CreateMenuElement(new String("Работа с файлами"),
-        new String[]{"Формат TXT", "Формат World", "Формат Excel","Формат CSV"},
-        new String[]{"Записать в файл", "Дописать в файл", "Чтение из файла"});
+                new String[] { "Формат TXT", "Формат World", "Формат Excel", "Формат CSV" },
+                new String[] { "Записать в файл", "Дописать в файл", "Чтение из файла" });
         elements.add(openFile);
 
-        //  TopMenuElement showReports = new TopMenuElement(0, 0);
-        //  TopMenuElement options = new TopMenuElement(0, 4);
-        //  TopMenuElement exit = new TopMenuElement(0, 0);
+        TopMenuElement showReports = new TopMenuElement(0, 0);
+        showReports.CreateMenuElement(new String("Отчёты"), new String[] {}, new String[] {});
+        elements.add(showReports);
 
+        TopMenuElement options = new TopMenuElement(0, 4);
+        options.CreateMenuElement("Администрирование",
+                new String[] {},
+                new String[] { "Настройки", "Пользователи", "Создание архива", "Восстановление из архива" });
+        elements.add(options);
+
+        TopMenuElement exit = new TopMenuElement(0, 0);
+        exit.CreateMenuElement(new String("Выход"), new String[] {}, new String[] {});
+        elements.add(exit);
 
         for (TopMenuElement topMenuElement : elements) {
+            System.out.println(topMenuElement.toString());
             menuBar.add(topMenuElement.getjMenu());
         }
     }
--- TopMenuElement.java
+++ TopMenuElement.java
@@ -6,47 +6,55 @@ public class TopMenuElement {
     JMenu mainMeJMenu;
     JMenu[] jMenus;
     JMenuItem[] items;
+    String _name;
 
-    public TopMenuElement(int jMenusElements, int itemElements){
-     this.jMenus = new JMenu[jMenusElements];  
-     this.items = new JMenuItem[itemElements]; 
+    public TopMenuElement(int jMenusElements, int itemElements) {
+        this.jMenus = new JMenu[jMenusElements];
+        this.items = new JMenuItem[itemElements];
     }
 
     public JMenu getjMenu() {
         return mainMeJMenu;
     }
 
-    public int CreateMenuElement(String name,String[] menuName, String[] itemsName){
+    public int CreateMenuElement(String name, String[] menuName, String[] itemsName) {
 
-        if ((menuName.length != jMenus.length) || (itemsName.length != items.length)) //Если поля не заданы, прервыаем метод
+        if ((menuName.length != jMenus.length) || (itemsName.length != items.length)) // Если поля не заданы, прервыаем
+                                                                                      // метод
             return 0;
-        
-        mainMeJMenu = new JMenu(name); //Создаем главную вкладку меню
 
-        for(int i = 0; i < itemsName.length; i++){   //Создаем подэлементы меню
-            items[i] = new JMenuItem(itemsName[i]);
-        }
+        mainMeJMenu = new JMenu(name); // Создаем главную вкладку меню
+        _name= name;
 
-        //Если кол-во параметров меню = 0 следующий блок не сработает
-        if(jMenus.length >= 1){
+        // Если кол-во параметров меню != 0
+        if (jMenus.length >= 1) {
             for (int i = 0; i < jMenus.length; i++) { // Создаем параметры меню
                 jMenus[i] = new JMenu(menuName[i]);
             }
             for (int i = 0; i < jMenus.length; i++) { // Добавляем подэлементы в парамерты меню
-                for(int j = 0; j < items.length; j++){
-                    jMenus[i].add(items[j]);
+                for (int j = 0; j < items.length; j++) {
+                    jMenus[i].add(new JMenuItem(itemsName[j]));
                 }
             }
-            for (JMenu jMenu : jMenus) { //Добавлем параметры меню в основное меню
+            for (JMenu jMenu : jMenus) { // Добавлем параметры меню в основное меню
                 mainMeJMenu.add(jMenu);
             }
         }
 
-        if(jMenus.length == 0){     //Если есть только один элемент меню, то добавляем в него действия
+        if (jMenus.length == 0) { // Если есть только один элемент меню, то добавляем в него действия
+
+            for (int i = 0; i < itemsName.length; i++) { // Создаем подэлементы меню
+                items[i] = new JMenuItem(itemsName[i]);
+            }
             for (JMenuItem jMenuItem : items) {
                 mainMeJMenu.add(jMenuItem);
             }
         }
         return 1;
+    }
+
+    @Override
+    public String toString() {
+        return _name;
     }
 }
